ad02d8a297451c33570dd86bc749b874eabde7d9
Lucas Silva <lucascitty2@gmail.com>
2024-06-19 16:37:33 -0300

    funcionalidade de galeria

--- js/script.js
+++ js/script.js
@@ -16,37 +16,53 @@ links.forEach(ativarLink);
 const parametros = new URLSearchParams(location.search);
 
 function ativarProduto(parametro) {
-  const elemento = document.getElementById(parametro)
-  if(elemento) {
-    elemento.checked = true
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
   }
-  console.log(elemento)
+  console.log(elemento);
 }
 
-parametros.forEach(ativarProduto)
-
+parametros.forEach(ativarProduto);
 
 // Perguntas Frequentes
 
-const perguntas = document.querySelectorAll('.perguntas button')
+const perguntas = document.querySelectorAll(".perguntas button");
 
 function ativarPergunta(ev) {
-  const pergunta = ev.currentTarget
-  const controls = pergunta.getAttribute('aria-controls')
-  const resposta = document.getElementById(controls)
+  const pergunta = ev.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
 
-  resposta.classList.toggle('ativa')
-  const ativa = resposta.classList.contains('ativa')
-  pergunta.setAttribute('aria-expanded', ativa)
-  console.log(resposta)
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+  console.log(resposta);
 }
 
 function eventosPerguntas(pergunta) {
-  pergunta.addEventListener('click', ativarPergunta)
+  pergunta.addEventListener("click", ativarPergunta);
 }
 
-if(perguntas){
-  perguntas.forEach(eventosPerguntas)
+if (perguntas) {
+  perguntas.forEach(eventosPerguntas);
 }
 
-console.log(perguntas)
+// Galeria de Bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(ev) {
+  const img = ev.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
